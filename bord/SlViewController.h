fa06349c552170742a0de6d7bb96aea80c3a213e
Martin Lizon <martin_lizon@xyratex.com>
2014-05-07 09:59:30 -0400

    Task: Getting a photo into an app
    This change provides basic functionality of getting a photo into an app.
    It uses a Navigation Controller UI widget, and the SlViewController drives that interaction.
    Currently:
    - a default image will appear (spaceship.png) the first time that the app is loaded by the user.
    - the user can use the 'camera' button on the top left to choose images from the photo gallery. Presumably, if a camera is present, the user can take a picture and load it in as well.
    - once the picture is loaded, it is saved into the apps "Documents" folder. When the app is restarted and a saved image exists, it will be loaded (instead of the default 'spaceship.png').
    
    This commit needs to be tested on another machine to ensure that all committed sources are also the required sources for compilation on another instance of Xcode.
    
    Signed-off-by: Martin Lizon <martin_lizon@xyratex.com>

--- bord/SlViewController.h
+++ bord/SlViewController.h
@@ -9,6 +9,15 @@
 #import <UIKit/UIKit.h>
 #import <SpriteKit/SpriteKit.h>
 
-@interface SlViewController : UIViewController
+@interface SlViewController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+{
+    UIScrollView *filtersScrollView;
+    UIImage *finalImage;
+}
+
+
+@property (strong, nonatomic) IBOutlet UIImageView *myImageView;
+
+@property (strong, nonatomic) IBOutlet UIToolbar *pick_picture_toolbar;
 
 @end
--- bord/SlViewController.m
+++ bord/SlViewController.m
@@ -11,22 +11,45 @@
 
 @implementation SlViewController
 
+@synthesize myImageView,pick_picture_toolbar;
+
+NSString *const SAVED_IMAGE = @"/current_slapping_image.jpg";
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+    filtersScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 300, self.view.bounds.size.width, 90)];
     
-    // Create and configure the scene.
-    SKScene * scene = [SlMyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
+    [filtersScrollView setScrollEnabled:YES];
+    [filtersScrollView setShowsVerticalScrollIndicator:NO];
+    filtersScrollView.showsHorizontalScrollIndicator = NO;
     
-    // Present the scene.
-    [skView presentScene:scene];
+    UIBarButtonItem *cameraBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(pickPicture:)];
+    
+    [[self navigationItem] setRightBarButtonItem:cameraBarButtonItem];
+    
+    [self.view addSubview:filtersScrollView];
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
+    NSString *docs = [paths objectAtIndex:0];
+    NSString* imageName =  [docs stringByAppendingFormat:SAVED_IMAGE];
+    NSLog(@"FFF: %@", imageName);
+    //./C45C9292-D3DF-4A6D-8AB2-8D7CF426D96B/bord.app/Spaceship.png
+    UIImage* imageObj = [[UIImage alloc] initWithContentsOfFile:imageName];
+    
+    if (imageObj) {
+        // set the image vie to the image from file, if it exists (otherwise load default).
+        [self.myImageView setImage:imageObj];
+    }
 }
+
 
 - (BOOL)shouldAutorotate
 {
@@ -42,10 +65,45 @@
     }
 }
 
-- (void)didReceiveMemoryWarning
+-(void) pickPicture:(id) sender
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc]init];
+    
+    NSLog(@"MARFOO1");
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        [imagePickerController setSourceType:UIImagePickerControllerSourceTypeCamera];
+    }
+    else
+    {
+        [imagePickerController setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }
+    
+    [imagePickerController setDelegate:self];
+    
+    [self presentModalViewController:imagePickerController animated:YES];
+}
+
+-(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSLog(@"FOO2");
+    finalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self.myImageView setImage:finalImage];
+    
+    // save image
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
+    NSString *docs = [paths objectAtIndex:0];
+    NSString* path =  [docs stringByAppendingFormat:SAVED_IMAGE];
+    NSLog(@"FFF: %@", path);
+    
+    NSData* imageData = [NSData dataWithData:UIImageJPEGRepresentation(self.myImageView.image, 80)];
+    NSError *writeError = nil;
+    BOOL result = [imageData writeToFile:path options:NSDataWritingAtomic error:&writeError];
+    
+    if(writeError!=nil) {
+        NSLog(@"%@: Error saving image: %@", [self class], [writeError localizedDescription]);
+    }
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
